c578626438be21bc75bd0d82c1ca3534f2a24ddd
Ciprian Dorin Craciun <ciprian@volution.ro>
2014-11-15 16:24:05 +0200

    Update scripts in-line with latest mOS changes

--- scripts/run-boot.bash
+++ scripts/run-boot.bash
@@ -5,14 +5,26 @@ if ! test "${#}" -eq 0 ; then
 	exit 1
 fi
 
-if test -n "${mosaic_application_fqdn:-}" ; then
+# FIXME: Remove this hack!
+if test "${UID}" -eq 0 ; then
+	find /mos -mindepth 1 -maxdepth 1 -type d -exec chmod 1777 -- {} \;
+fi
+
+if test "$( getent passwd -- mos-services | cut -f 3 -d : )" -ne "${UID}" ; then
+	exec sudo -u mos-services -g mos-services -E -n -- "${0}" "${@}"
+	exit 1
+fi
+
+umask 0022
+
+if test -n "${mosaic_cluster_nodes_fqdn:-}" ; then
 	true
-elif test -n "${mos_application_private_fqdn:-}" ; then
-	mosaic_application_fqdn="${mos_application_private_fqdn}"
-elif test -n "${mos_application_public_fqdn:-}" ; then
-	mosaic_application_fqdn="${mos_application_public_fqdn}"
+elif test -n "${mos_cluster_nodes_private_fqdn:-}" ; then
+	mosaic_cluster_nodes_fqdn="${mos_cluster_nodes_private_fqdn}"
+elif test -n "${mos_cluster_nodes_public_fqdn:-}" ; then
+	mosaic_cluster_nodes_fqdn="${mos_cluster_nodes_public_fqdn}"
 else
-	mosaic_application_fqdn=
+	mosaic_cluster_nodes_fqdn=
 fi
 
 if test -n "${mosaic_node_fqdn:-}" ; then
@@ -47,19 +59,28 @@ fi
 if test -n "${mosaic_node_temporary:-}" ; then
 	true
 elif test -n "${mos_fs_tmp:-}" ; then
-	mosaic_node_temporary="${mos_fs_tmp}/platform"
+	mosaic_node_temporary="${mos_fs_tmp}/mosaic/node"
 else
 	echo "[ww] missing node temporary; falling to defaults..." >&2
-	mosaic_node_temporary="/tmp/mosaic/platform"
+	mosaic_node_temporary="/tmp/mosaic/node"
+fi
+
+if test -n "${mosaic_node_tmpdir:-}" ; then
+	true
+elif test -n "${mos_fs_tmp:-}" ; then
+	mosaic_node_tmpdir="${mos_fs_tmp}"
+else
+	echo "[ww] missing node TMPDIR; falling to defaults..." >&2
+	mosaic_node_tmpdir="/tmp"
 fi
 
 if test -n "${mosaic_node_log:-}" ; then
 	true
 elif test -n "${mos_fs_log:-}" ; then
-	mosaic_node_log="${mos_fs_log}/platform.log"
+	mosaic_node_log="${mos_fs_log}/mosaic/node/node.log"
 else
 	echo "[ww] missing node log; falling to defaults..." >&2
-	mosaic_node_log="${mosaic_node_temporary}/platform.log"
+	mosaic_node_log="${mosaic_node_temporary}/node.log"
 fi
 
 if test -n "${mosaic_node_path:-}" ; then
@@ -68,38 +89,53 @@ else
 	mosaic_node_path="${_PATH}"
 fi
 
-echo "[ii] using the application FQDN \`${mosaic_application_fqdn}\`;" >&2
+if test -n "${mosaic_node_home:-}" ; then
+	true
+else
+	mosaic_node_home="${mosaic_node_temporary}/home"
+fi
+
+echo "[ii] using the cluster FQDN \`${mosaic_cluster_nodes_fqdn}\`;" >&2
 echo "[ii] using the node FQDN \`${mosaic_node_fqdn}\`;" >&2
 echo "[ii] using the node IP \`${mosaic_node_ip}\`;" >&2
 echo "[ii] using the node log \`${mosaic_node_log}\`;" >&2
 echo "[ii] using the node definitions \`${mosaic_node_definitions}\`;" >&2
 echo "[ii] using the node temporary \`${mosaic_node_temporary}\`;" >&2
+echo "[ii] using the node TMPDIR \`${mosaic_node_tmpdir}\`;" >&2
 echo "[ii] using the node PATH \`${mosaic_node_path}\`;" >&2
+echo "[ii] using the node HOME \`${mosaic_node_home}\`;" >&2
 
 if test ! -e "${mosaic_node_temporary}" ; then
 	mkdir -p -- "${mosaic_node_temporary}"
+fi
+
+if test ! -e "$( dirname -- "${mosaic_node_log}" )" ; then
+	mkdir -p -- "$( dirname -- "${mosaic_node_log}" )"
 fi
 
 if test ! -e "${mosaic_node_temporary}/home" ; then
 	mkdir -p -- "${mosaic_node_temporary}/home"
 fi
 
-if test ! -e "${mosaic_node_temporary}/.iptables" ; then
-	touch "${mosaic_node_temporary}/.iptables"
-	iptables -t nat -A PREROUTING -p tcp --dport 80 -m state --state NEW -j DNAT --to :31000 2>/dev/null || true
+if test ! -e "${mosaic_node_temporary}/cwd" ; then
+	mkdir -p -- "${mosaic_node_temporary}/cwd"
 fi
 
 _exec_env=(
-		mosaic_application_fqdn="${mosaic_application_fqdn}"
+		mosaic_cluster_nodes_fqdn="${mosaic_cluster_nodes_fqdn}"
 		mosaic_node_fqdn="${mosaic_node_fqdn}"
 		mosaic_node_ip="${mosaic_node_ip}"
 		mosaic_node_log="${mosaic_node_log}"
 		mosaic_node_definitions="${mosaic_node_definitions}"
 		mosaic_node_temporary="${mosaic_node_temporary}"
 		mosaic_node_path="${mosaic_node_path}"
-		HOME="${mosaic_node_temporary}/home"
+		mosaic_node_home="${mosaic_node_home}"
+		TMPDIR="${mosaic_node_tmpdir}"
 		PATH="${mosaic_node_path}"
+		HOME="${mosaic_node_home}"
 )
+
+cd -- "${mosaic_node_temporary}/cwd"
 
 if test "${#}" -eq 0 ; then
 	exec env -i "${_exec_env[@]}" mosaic-node--run-node
